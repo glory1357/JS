0900320d8a6796fdd2b26581b3ca106843e77aba
glory1357 <efim133@mail.ru>
2021-08-26 21:30:35 +0300

    coctails

--- Task2/JS/HashStorage.js
+++ Task2/JS/HashStorage.js
@@ -1,3 +1,22 @@
-function HashStorage(){
-    
+function HashStorage() {
+    var self = this;
+    self.storage = {};
+    self.addValue = function(key, value) {
+        self.storage[key] = value;
+    }
+    self.getValue = function(key) {
+        return self.storage[key];
+    }
+    self.deleteValue = function(key) {
+        if (key in self.storage) {
+            return delete self.storage[key];
+        } else {
+            return false;
+        }
+    }
+    self.getKeys = function() {
+        for (var key in self.storage) {
+            return Object.keys(self.storage);
+        }
+    }
 }
